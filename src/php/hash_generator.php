6f73924285488c07582a63cdf438c058275c128f
mlsfront <mlsfront@outlook.com>
2025-09-20 11:00:52 -0300

    feat(bcrypt): Incluindo opção Bcrypt --modified:   src/index.html --modified:   src/js/scripts.js --modified:   src/php/hash_generator.php

--- src/php/hash_generator.php
+++ src/php/hash_generator.php
@@ -2,19 +2,19 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = json_decode(file_get_contents("php://input"), true);
     $password = $data['password'] ?? '';
-
     if (empty($password)) {
         echo json_encode(['error' => 'A senha é obrigatória.']);
         exit;
     }
-
     $md5Hash = md5($password);
     $sha1Hash = sha1($password);
-
+    $bcryptHash = password_hash($password, PASSWORD_BCRYPT);
     echo json_encode([
         'password' => $password,
         'md5' => $md5Hash,
-        'sha1' => $sha1Hash
+        'sha1' => $sha1Hash,
+        'bcrypt' => $bcryptHash
     ]);
 }
 ?>
+
